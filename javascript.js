f744cdf2537eeff30d290ca5d1b1c61325c4f7c2
Mamatha Munivelu <munivelu.mamatha@accenture.com>
2012-11-29 16:02:38 +0530

    yes

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ function init() {
 function GetProfile()
 {
   osapi.jive.corev3.people.get({id : '@me'}).execute(function(response) {
-        console.log("The User is " + JSON.stringify(response));
+        console.log("The User is " + JSON.stringify(response.data));
 		});
 		 gadgets.window.adjustHeight();
 }	  
