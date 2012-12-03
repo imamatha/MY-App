f1b5841957a33da90cff419ede8e28ca917cedc6
Mamatha Munivelu <munivelu.mamatha@accenture.com>
2012-12-03 10:28:08 +0530

    yes

---
+++ new  4.js
@@ -0,0 +1,100 @@
+The User is {
+    "type": "person",
+    "followingCount": 2,
+    "resources": {
+        "reports": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/@reports"
+        },
+        "manager": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/@manager"
+        },
+        "self": {
+            "allowed": ["GET", "PUT"],
+            "ref": "/people/2130"
+        },
+        "tasks": {
+            "allowed": ["POST", "GET"],
+            "ref": "/people/2130/tasks"
+        },
+        "avatar": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/avatar"
+        },
+        "colleagues": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/@colleagues"
+        },
+        "followers": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/@followers"
+        },
+        "following": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/@following"
+        },
+        "images": {
+            "allowed": ["POST", "GET"],
+            "ref": "/people/2130/images"
+        },
+        "html": {
+            "allowed": ["GET"],
+            "ref": "https://apps-public-cloud-trunk.jivesoftware.com/people/mamatha.munivelu"
+        },
+        "streams": {
+            "allowed": ["POST", "GET"],
+            "ref": "/people/2130/streams"
+        },
+        "followingIn": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/followingIn"
+        },
+        "members": {
+            "allowed": ["GET"],
+            "ref": "/members/people/2130"
+        },
+        "activity": {
+            "allowed": ["GET"],
+            "ref": "/people/2130/activities"
+        }
+    },
+    "status": "@working on new sandbox.",
+    "emails": [{
+        "primary": true,
+        "value": "munivelu.mamatha@accenture.com",
+        "type": "work",
+        "jive_label": "Email"
+    }],
+    "id": "2130",
+    "jive": {
+        "enabled": true,
+        "username": "mamatha.munivelu",
+        "level": {
+            "name": "Wayfarer",
+            "points": 50
+        },
+        "visible": true,
+        "external": false,
+        "externalContributor": false,
+        "federated": false,
+        "profile": [{
+            "value": "",
+            "jive_label": "Biography"
+        }, {
+            "value": "Interests and expertise",
+            "jive_label": "Expertise"
+        }]
+    },
+    "updated": "2012-11-29T08:42:16.616+0000",
+    "name": {
+        "formatted": "Mamatha Munivelu",
+        "familyName": "Munivelu",
+        "givenName": "Mamatha"
+    },
+    "thumbnailUrl": "/people/2130/avatar",
+    "displayName": "Mamatha Munivelu",
+    "published": "2012-11-28T12:10:11.211+0000",
+    "followerCount": 0
+	
+}
